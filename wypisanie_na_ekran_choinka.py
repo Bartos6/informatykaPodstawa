fbc356454b045b1d3816a5318fb6e3c1f65b9ff0
Bartos6 <130593832+Bartos6@users.noreply.github.com>
2025-09-24 15:42:11 +0200

    Create wypisanie_na_ekran_choinka.py

---
+++ wypisanie_na_ekran_choinka.py
@@ -0,0 +1,18 @@
+#  ZADANIE choinka z *
+print("  *")
+print(" ***")
+print("*****")
+print("  *")
+
+#ZADANIE choinka z liczby 0
+print(" ", 0)
+print(" ", 0, 0, 0, sep = "")
+print(0, 0, 0, 0, 0, sep = "")
+print("  ", 0, sep = "")
+
+#ZADANIE choinka ze zmiennej a
+a = 5
+print(" ", a)
+print(" ", a, a, a, sep = "")
+print(a, a, a, a, a, sep = "")
+print("  ", a, sep = "")
