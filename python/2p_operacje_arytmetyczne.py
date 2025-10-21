f62d28ea7e20d8d3bd3c78f7045db9e87aa65c8e
Bartos6 <130593832+Bartos6@users.noreply.github.com>
2025-10-21 15:32:56 +0200

    Refactor calculator script and add new functionalities

--- python/2p_operacje_arytmetyczne.py
+++ python/2p_operacje_arytmetyczne.py
@@ -1,4 +1,4 @@
-#ZADANIE kalkulator
+#####ZADANIE kalkulator
 a = int(input("Podaj a: "))
 b = int(input("Podaj b: "))
 
@@ -13,7 +13,6 @@ roznica = a - b
 print("Roznica:", roznica)
 
 #Wypisanie całego działania
-
 print(a, "*", b, "=", a*b)
 print(a, "/", b, "=", a/b)
 print(a, "//", b, "=", a//b)
@@ -21,3 +20,16 @@ print(a, "%", b, "=", a%b)
 
 #extra
 print(a, ":", b, "=", a//b, "r.", a%b)
+
+
+#####ZADANIE Pole trójkąta 
+a = int(input("Podaj a: "))
+h = int(input("Podaj h: "))
+
+print("Pole trojkata: ", a*h/2)
+
+#####ZADANIE Zamiana temperatur
+c = int(input("Podaj c: "))
+
+print("Temperatura w Fahrenheitach: ", 9/5 * c + 32)
+
